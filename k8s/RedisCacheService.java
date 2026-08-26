eae351c5499b85ab2c4e588095928f9e57aa7de9
MacawParrot-g <18165764261@163.com>
2026-08-26 15:32:52 +0800

    Delete k8s directory

--- k8s/RedisCacheService.java
+++
@@ -1,8 +0,0 @@
-package org.example.service;
-
-public interface RedisCacheService {
-    boolean exists(String url, String bundleId);
-    void save(String url, String bundleId);
-    boolean isAvailable();
-    void warmUpFromMySQL(java.util.List<org.example.entity.TestStatic> records);
-}
