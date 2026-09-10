16c3ac4c4dc6344cce27030690e178ec96db41e6
MacawParrot-g <18165764261@163.com>
2026-09-10 17:39:52 +0800

    Delete backend/src/main/java/org/example directory

--- backend/src/main/java/org/example/service/AppIdService.java
+++
@@ -1,7 +0,0 @@
-package org.example.service;
-
-public interface AppIdService {
-    boolean isAppIdExists(Long appId);
-    void saveAppId(String bundleId, Long appId);
-    void warmUpAppIdCache();
-}
