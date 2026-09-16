0f9e65675ffde528e2f279547cb65a503f05e9ed
MacawParrot-g <18165764261@163.com>
2026-09-16 10:16:06 +0800

    Delete backend/src/main/java/org/example directory

--- backend/src/main/java/org/example/service/LocalCacheService.java
+++
@@ -1,6 +0,0 @@
-package org.example.service;
-
-public interface LocalCacheService {
-    boolean exists(String downloadUrl, String bundleId);
-    void save(String downloadUrl, String bundleId);
-}
