7c7fc6c1b7e8e5855a151134d44f7f063d5eefce
MacawParrot-g <18165764261@163.com>
2026-09-17 09:30:28 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     // 核心修改：将输出目录指向 nginx/html/dist
     // '../nginx/html/dist' 表示向上一级目录走，进入 nginx/html/dist
-    outDir: path.resolve(__dirname, '../nginx/html/dist'),
+    outDir: 'dist',
     emptyOutDir: true, // 每次构建前清空该目录，防止旧文件残留
   },
 })
